a63aa0dfb2ff167eda4f1075c1c10377e79d83fb
fuli <fuli@rockx.com>
2022-02-21 16:00:38 +0800

    update contracts

--- eth2staking.sol
+++ eth2staking.sol
@@ -21,8 +21,9 @@ contract ETH2Staking is IETH2Staking, ReentrancyGuard, Pausable, Ownable {
     using SafeMath for uint256;
 
     uint256 internal constant NODE_ETH_LIMIT = 32 ether;
-    uint256 internal constant SHARE_MULTIPLIER = 1e18; 
+    uint256 internal constant MULTIPLIER = 1e18; 
 
+    address public xETHAddress; // xETH token address
     address public managerAccount;
     uint256 public managerFeeMilli = 100; // *1/1000
 
@@ -30,24 +31,28 @@ contract ETH2Staking is IETH2Staking, ReentrancyGuard, Pausable, Ownable {
         // fields related before validator setup
         mapping(address=>uint256) accounts;
         uint256 totalEthers;
-
-        // fields after validator has spinned up
-        mapping(address=>uint256) rewardDebts;
-        uint256 accEthersPerShare;
-
         bool hasWithdrawed;
     }
 
+    // validator start up related;
     address public nextValidator; // still waiting for ether deposits
     mapping(address=>Validator) internal nodes; // spined up nodes
     address [] public validators;
     uint256 public numValidators;
 
+    // revenue distribution related
+    uint256 totalStaked;
+    uint256 totalRevenue;
+
+    // pending withdraw
+    mapping(address=>uint256) pendingRedeem;
+
     /**
      * Global
      */
 
-    constructor() public {
+    constructor(address xETHAddress_) public {
+        xETHAddress = xETHAddress_;
         managerAccount = msg.sender;
         nextValidator = address(new ETH2Validator());
         validators.push(nextValidator);
@@ -83,11 +88,9 @@ contract ETH2Staking is IETH2Staking, ReentrancyGuard, Pausable, Ownable {
         uint256 fee = msg.value.mul(managerFeeMilli).div(1000);
 
         payable(managerAccount).sendValue(fee);
-        uint256 share = msg.value.sub(fee)
-                            .mul(SHARE_MULTIPLIER)
-                            .div(nodes[msg.sender].totalEthers);
-
-        nodes[msg.sender].accEthersPerShare = nodes[msg.sender].accEthersPerShare.add(share);
+        totalRevenue = msg.value
+                            .sub(fee)
+                            .add(totalRevenue);
 
         emit RevenueReceived(msg.value);
     }
@@ -100,37 +103,39 @@ contract ETH2Staking is IETH2Staking, ReentrancyGuard, Pausable, Ownable {
         return validators[validatorId].accountList;
     }
     */
-    function checkReward(address account, address validator) public view returns (uint256) {
-        Validator storage node = nodes[validator];
-        uint256 amount = node.accounts[account];
-        uint256 rewardDebt = node.rewardDebts[account];
 
-        uint256 revenue = amount
-                                .mul(node.accEthersPerShare.sub(rewardDebt))
-                                .div(SHARE_MULTIPLIER);
-        return revenue;
-    }
 
     /**
-     * claim rewards and update reward debts
-     */ 
-    function claimRewards(address validator) external nonReentrant {
-        uint256 rewards = checkReward(msg.sender, validator);
-        Validator storage node = nodes[validator];
-        node.rewardDebts[msg.sender] = node.accEthersPerShare;
-        msg.sender.sendValue(rewards);
+     * @dev return exchange ratio of xETH:ETH, multiplied by 1e18
+     */
+    function exchangeRatio() public view returns (uint256) {
+        uint256 xETHAmount = IERC20(xETHAddress).totalSupply();
+        uint256 ratio = totalStaked.add(totalRevenue)
+                            .mul(MULTIPLIER)
+                            .div(xETHAmount);
+        return ratio;
     }
  
-    function deposit() external 
+    /**
+     * @dev mint xETH with ETH
+     */
+    function mint() external 
         payable 
         nonReentrant 
         whenNotPaused 
     {
         require(msg.value > 0, "amount 0");
 
-        // allocate eth to validators
-        uint256 ethersRemain = msg.value;
-        
+        // mint xETH while keep the exchange ratio invariant
+        //
+        // amount XETH to mint = xETH * (current_ethers + ethers_to_deposit)/current_ethers - xETH
+        //
+        uint256 amountXETH = IERC20(xETHAddress).totalSupply();
+        uint256 currentEthers = totalStaked.add(totalRevenue);
+        uint256 toMint = amountXETH.mul(currentEthers.add(msg.value)).div(currentEthers).sub(amountXETH);
+
+        // allocate ethers to validators
+        uint256 ethersRemain = msg.value;        
         while (ethersRemain > 0) {
             Validator storage node = nodes[nextValidator];
             if (node.totalEthers.add(ethersRemain) >= NODE_ETH_LIMIT) {
@@ -149,6 +154,33 @@ contract ETH2Staking is IETH2Staking, ReentrancyGuard, Pausable, Ownable {
                 ethersRemain = 0;
             }
         }
+
+        // sum total ethers
+        totalStaked = totalStaked.add(msg.value);
+        // mint xETH
+        IMintableContract(xETHAddress).mint(msg.sender, toMint);
+    }
+
+    /**
+     * @dev redeem ETH by burning xETH with current exchange ratio, 
+     * approve xETH to this contract first
+     */
+    function redeemUnderlying(uint256 ethersToRedeem) external nonReentrant {
+        // amount XETH to burn = xETH - xETH * (current_ethers - ethers_to_redeem)/current_ethers
+        // redeem keeps the ratio invariant
+        uint256 amountXETH = IERC20(xETHAddress).totalSupply();
+        uint256 currentEthers = totalStaked.add(totalRevenue);
+        uint256 toBurn = amountXETH.sub
+                            (
+                                amountXETH.mul(currentEthers.sub(ethersToRedeem)).div(currentEthers)
+                            );
+        
+        // transfer xETH from sender
+        IERC20(xETHAddress).safeTransferFrom(msg.sender, address(this), toBurn);
+        // burn
+        IMintableContract(xETHAddress).burn(toBurn);
+        // send ethers back to sender
+        msg.sender.sendValue(ethersToRedeem);
     }
 
     // spin up the node
@@ -160,17 +192,8 @@ contract ETH2Staking is IETH2Staking, ReentrancyGuard, Pausable, Ownable {
         nextValidator = address(new ETH2Validator());
         validators.push(nextValidator);
         numValidators = validators.length;
-    }
 
-    // withdraw ethers in a node
-    function withdraw(address validator) external nonReentrant onlyOwner {
-        Validator storage node = nodes[validator];
-        require(!node.hasWithdrawed);
-        require(node.totalEthers >= NODE_ETH_LIMIT);
-
-        node.hasWithdrawed = true;
-        
-        msg.sender.sendValue(node.totalEthers);
+        // TODO: deposit to ethereum contract
     }
 
     /**
@@ -180,4 +203,5 @@ contract ETH2Staking is IETH2Staking, ReentrancyGuard, Pausable, Ownable {
     event RevenueReceived(uint256 amount);
     event ManagerAccountSet(address account);
     event ManagerFeeSet(uint256 milli);
+    event Withdrawed(address validator);
 }
