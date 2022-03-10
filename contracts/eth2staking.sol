d6b6ea138d67f5319d6de2b6c63bc0d6730a45a2
fuli <fuli@rockx.com>
2022-03-10 17:17:23 +0800

    import openzepplin directly

--- contracts/eth2staking.sol
+++ contracts/eth2staking.sol
@@ -3,7 +3,11 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./library.sol";
 import "solidity-bytes-utils/contracts/BytesLib.sol";
-
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract ETH2Staking is ReentrancyGuard, Pausable, Ownable {
     using SafeERC20 for IERC20;
