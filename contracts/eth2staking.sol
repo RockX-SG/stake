85a574206b801bef3a1056dafd1336694b12ab46
fuli <fuli@rockx.com>
2022-03-11 23:21:34 +0800

    update import

--- contracts/eth2staking.sol
+++ contracts/eth2staking.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./library.sol";
+import "./iface.sol";
 import "solidity-bytes-utils/contracts/BytesLib.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
