d8229ce2b11a878f76300cc82becca72e7a6dbc2
fuli <fuli@rockx.com>
2022-03-23 12:36:26 +0800

    make xETH upgradeable

--- contracts/xETH.sol
+++ contracts/xETH.sol
@@ -1,19 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
-contract xETH is ERC20, ERC20Snapshot, Ownable, Pausable {    
-    using SafeERC20 for IERC20;
-    using SafeMath for uint;
-    using Address for address payable;
-    using SafeMath for uint256;
-    
+contract xETH is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable {    
    /**
      * @dev Emitted when an account is set mintable
      */
@@ -30,14 +25,17 @@ contract xETH is ERC20, ERC20Snapshot, Ownable, Pausable {
         require(mintableGroup[msg.sender], "xETH: not in mintable group");
         _;
     }
-    
-    /**
-     * @dev Initialize the contract give all tokens to the deployer
-     */
-    constructor() ERC20("RockX ETH", "xETH") {
+
+    function initialize() initializer public {
+        __ERC20_init("RockX ETH", "xETH");
+        __ERC20Burnable_init();
+        __ERC20Snapshot_init();
+        __Ownable_init();
+        __Pausable_init();
+
         setMintable(owner(), true); // default mintable at constructor
     }
-    
+
     /**
      * @dev set or remove address to mintable group
      */
@@ -51,40 +49,34 @@ contract xETH is ERC20, ERC20Snapshot, Ownable, Pausable {
             emit Unmintable(account);
         }
     }
-    
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements:
-     *
-     * - `to` cannot be the zero address.
-     */
-    function mint(address account, uint256 amount) public onlyMintableGroup {
-        _mint(account, amount);
-        
-    }
+
     /**
      * @dev Destroys `amount` tokens from the user.
      *
      * See {ERC20-_burn}.
      */
-    function burn(uint256 amount) public onlyMintableGroup {
+    function burn(uint256 amount) public override onlyMintableGroup {
         _burn(_msgSender(), amount);
     }
 
     /**
-     * @dev Batch transfer amount to recipient
-     * @notice that excessive gas consumption causes transaction revert
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
      */
-    function batchTransfer(address[] memory recipients, uint256[] memory amounts) public {
-        require(recipients.length > 0, "xETH: least one recipient address");
-        require(recipients.length == amounts.length, "xETH: number of recipient addresses does not match the number of tokens");
+    function burnFrom(address account, uint256 amount) public override onlyMintableGroup {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
 
-        for(uint256 i = 0; i < recipients.length; ++i) {
-            _transfer(_msgSender(), recipients[i], amounts[i]);
-        }
+    function mint(address to, uint256 amount) public onlyMintableGroup {
+        _mint(to, amount);
     }
 
     function snapshot() public onlyOwner {
@@ -102,9 +94,22 @@ contract xETH is ERC20, ERC20Snapshot, Ownable, Pausable {
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
         whenNotPaused
-        override(ERC20, ERC20Snapshot)
+        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
     {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * @dev Batch transfer amount to recipient
+     * @notice that excessive gas consumption causes transaction revert
+     */
+    function batchTransfer(address[] memory recipients, uint256[] memory amounts) public {
+        require(recipients.length > 0, "xETH: least one recipient address");
+        require(recipients.length == amounts.length, "xETH: number of recipient addresses does not match the number of tokens");
+
+        for(uint256 i = 0; i < recipients.length; ++i) {
+            _transfer(_msgSender(), recipients[i], amounts[i]);
+        }
     }
 }
 
