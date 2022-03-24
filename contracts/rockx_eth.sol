f92cb7b2c51e76ffaef6b9e7cb9007ee0863120f
fuli <fuli@rockx.com>
2022-03-24 21:39:54 +0800

    rename contract

--- contracts/rockx_eth.sol
+++ contracts/rockx_eth.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
-contract xETH is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable {    
+contract RockXETH is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable {    
    /**
      * @dev Emitted when an account is set mintable
      */
