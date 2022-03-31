335ffe9b11cb1ec88aa97febaa7cb65f5a5c3f18
fuli <fuli@rockx.com>
2022-03-31 18:04:10 +0800

    update config

--- contracts/scripts/test_deploy.py
+++ contracts/scripts/test_deploy.py
@@ -3,6 +3,7 @@ from brownie import (
         RockXStaking,
         accounts,
         chain,
+        interface
 )
 
 GAS_LIMIT = 6721975
@@ -10,7 +11,8 @@ GAS_LIMIT = 6721975
 def main():
     owner = accounts[0]
     admin = owner
-    ethDepositContract = "0x00000000219ab540356cBB839Cbe05303d7705Fa"
+    ethDepositContract = "0x00000000219ab540356cbb839cbe05303d7705fa"
+    withdrawalCredential = "0x00a7ea5e6d29ffaf8e80a35f419dd1603c5575f9d68ed75392a356ca17084c90"
 
     print(f'contract owner account: {owner.address}\n')
 
@@ -23,10 +25,14 @@ def main():
     )
    
     xETH_contract.initialize()
+    xETH_contract.setMintable(staking_contract, True)
     staking_contract.initialize()
-    staking_contract.setXETHContractAddress(xETH_contract.address)
+    staking_contract.setXETHContractAddress(xETH_contract)
     staking_contract.setETHDepositContract(ethDepositContract)
+    staking_contract.setWithdrawCredential(withdrawalCredential)
     staking_contract.registerValidator(
             0x97d717d346868b9df4851684d5219f4deb4c7388ee1454c9b46837d29b40150ceeb5825d791f993b03745427b6cbe6db, 
             0xa09b4dc28c10063f6e2a9d2ca94b23db029ef618660138898cb827eae227d99ee1c438988d0222ca4229ba85c40add3b045e823fdb7519a36538ff901ab89f311060bcecc517ba683b84009ee3509afbcd25e991ef34112a5a16be44265441eb)
+
+    
 
