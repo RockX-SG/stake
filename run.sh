2ef1a70dc7007596a198ce3db23779dceda71784
fuli <fuli@rockx.com>
2022-05-24 16:32:16 +0800

    upd run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-brownie networks add development mainnet-fork2 cmd=ganache-cli host=http://0.0.0.0 accounts=10 mnemonic=brownie port=8545 fork=mainnet
-brownie run ./scripts/test_deploy.py --network mainnet-fork2  -I
+brownie run ./scripts/ganache_deploy.py --network mainnet-fork  -I
