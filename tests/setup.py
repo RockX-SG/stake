a334015aedd1976feb2d6dcfc6f6db0297515e45
fuli <fuli@rockx.com>
2022-06-06 18:29:38 +0800

    add more tests

--- tests/setup.py
+++ tests/setup.py
@@ -7,7 +7,6 @@ from pathlib import Path
 GAS_LIMIT = 6721975
 deps = project.load(  Path.home() / ".brownie" / "packages" / config["dependencies"][0])
 
-@pytest.fixture
 def setup():
     TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
 
@@ -45,13 +44,8 @@ def setup():
             {'from': deployer, 'gas': GAS_LIMIT}
             )
 
-    global transparent_xeth
     transparent_xeth = Contract.from_abi("RockXETH", xETH_proxy.address, RockXETH.abi)
-
-    global transparent_staking
     transparent_staking = Contract.from_abi("RockXStaking",staking_proxy.address, RockXStaking.abi)
-
-    global transparent_redeem
     transparent_redeem = Contract.from_abi("RockXRedeem", redeem_proxy.address, RockXRedeem.abi)
 
     transparent_xeth.initialize(
@@ -93,53 +87,5 @@ def setup():
             0xa09b4dc28c10063f6e2a9d2ca94b23db029ef618660138898cb827eae227d99ee1c438988d0222ca4229ba85c40add3b045e823fdb7519a36538ff901ab89f311060bcecc517ba683b84009ee3509afbcd25e991ef34112a5a16be44265441eb,
         {'from': owner, 'gas': GAS_LIMIT}
     )
-
-def test_mint(setup):
-    user1 = accounts[2]
-    transparent_xeth.approve(transparent_staking, '100 ether', {'from': user1})
-    transparent_staking.mint(0, time.time() + 600, {'from':user1, 'value': "1 ether"})
-    assert transparent_staking.exchangeRatio() == 1e18
-
-def test_redeem(setup):
-    owner = accounts[0]
-    user1 = accounts[2]
-    transparent_staking.mint(0, time.time() + 600, {'from':user1, 'value': "32 ether"})
-    assert transparent_xeth.balanceOf(user1) == '32 ether'
-   
-    # initiate redeem and burn xETH
-    transparent_xeth.approve(transparent_staking, '100 ether', {'from': user1})
-    transparent_staking.redeemFromValidators("32 ether", "32 ether", time.time() + 600, {'from': user1})
-    assert transparent_staking.debtOf(user1) == '32 ether'
-    assert transparent_staking.exchangeRatio() == 1e18
-    assert transparent_xeth.balanceOf(user1) == 0
-
-    # mock 8 ethers received & validator stopped
-    assert transparent_staking.debtOf(user1) == '32 ether'
-    accounts[4].transfer(transparent_staking.address, '32 ethers')
-    vectorClock = transparent_staking.getVectorClock()
-    transparent_staking.syncBalance(vectorClock, {'from':owner})
-    vectorClock = transparent_staking.getVectorClock()
-    transparent_staking.validatorStopped([0x97d717d346868b9df4851684d5219f4deb4c7388ee1454c9b46837d29b40150ceeb5825d791f993b03745427b6cbe6db],'32 ethers', vectorClock, {'from':owner})
-    assert transparent_staking.debtOf(user1) == '0 ether'
-    assert transparent_redeem.balanceOf(user1) == '32 ether'
-
-    lastBalance = user1.balance()
-    transparent_redeem.claim('8 ether', {'from':user1})
-    assert transparent_redeem.balanceOf(user1) == '24 ether' 
-    assert user1.balance() - lastBalance == '8 ether'
-
-def test_beacon(setup):
-    expectedExchangeRatio = 1009950000000000000 
-    # some ethers to redeem
-    user1 = accounts[2]
-
-    oracle = accounts[3]
-    transparent_staking.mint(0, time.time() + 600, {'from':oracle, 'value': "32 ether"})
-    assert transparent_xeth.balanceOf(oracle) == '32 ether'
-
-    transparent_staking.grantRole(transparent_staking.ORACLE_ROLE(), oracle, {'from': accounts[0]})
-    vectorClock = transparent_staking.getVectorClock()
-    transparent_staking.pushBeacon(1, '32.32 ether', vectorClock, {'from':oracle})
-
-    assert transparent_staking.exchangeRatio() == expectedExchangeRatio
-
+    
+    return (transparent_xeth, transparent_staking, transparent_redeem)
---
+++ tests/test_staking.py
@@ -0,0 +1,58 @@
+import pytest
+import time
+
+from brownie import *
+import setup
+
+def test_mint():
+    transparent_xeth, transparent_staking, transparent_redeem = setup.setup()
+    user1 = accounts[2]
+    transparent_xeth.approve(transparent_staking, '100 ether', {'from': user1})
+    transparent_staking.mint(0, time.time() + 600, {'from':user1, 'value': "1 ether"})
+    assert transparent_staking.exchangeRatio() == 1e18
+
+def test_redeem():
+    transparent_xeth, transparent_staking, transparent_redeem = setup.setup()
+    owner = accounts[0]
+    user1 = accounts[2]
+    transparent_staking.mint(0, time.time() + 600, {'from':user1, 'value': "32 ether"})
+    assert transparent_xeth.balanceOf(user1) == '32 ether'
+   
+    # initiate redeem and burn xETH
+    transparent_xeth.approve(transparent_staking, '100 ether', {'from': user1})
+    transparent_staking.redeemFromValidators("32 ether", "32 ether", time.time() + 600, {'from': user1})
+    assert transparent_staking.debtOf(user1) == '32 ether'
+    assert transparent_staking.exchangeRatio() == 1e18
+    assert transparent_xeth.balanceOf(user1) == 0
+
+    # mock 8 ethers received & validator stopped
+    assert transparent_staking.debtOf(user1) == '32 ether'
+    accounts[4].transfer(transparent_staking.address, '32 ethers')
+    vectorClock = transparent_staking.getVectorClock()
+    transparent_staking.syncBalance(vectorClock, {'from':owner})
+    vectorClock = transparent_staking.getVectorClock()
+    transparent_staking.validatorStopped([0x97d717d346868b9df4851684d5219f4deb4c7388ee1454c9b46837d29b40150ceeb5825d791f993b03745427b6cbe6db],'32 ethers', vectorClock, {'from':owner})
+    assert transparent_staking.debtOf(user1) == '0 ether'
+    assert transparent_redeem.balanceOf(user1) == '32 ether'
+
+    lastBalance = user1.balance()
+    transparent_redeem.claim('8 ether', {'from':user1})
+    assert transparent_redeem.balanceOf(user1) == '24 ether' 
+    assert user1.balance() - lastBalance == '8 ether'
+
+def test_beacon():
+    transparent_xeth, transparent_staking, transparent_redeem = setup.setup()
+    expectedExchangeRatio = 1009950000000000000 
+    # some ethers to redeem
+    user1 = accounts[2]
+
+    oracle = accounts[3]
+    transparent_staking.mint(0, time.time() + 600, {'from':oracle, 'value': "32 ether"})
+    assert transparent_xeth.balanceOf(oracle) == '32 ether'
+
+    transparent_staking.grantRole(transparent_staking.ORACLE_ROLE(), oracle, {'from': accounts[0]})
+    vectorClock = transparent_staking.getVectorClock()
+    transparent_staking.pushBeacon(1, '32.32 ether', vectorClock, {'from':oracle})
+
+    assert transparent_staking.exchangeRatio() == expectedExchangeRatio
+
