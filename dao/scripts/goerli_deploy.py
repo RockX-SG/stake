16852ddd836a65bb5d29a2b4f2e555d5cf01b6c6
fuli <fuli@rockx.com>
2023-03-16 16:00:04 +0800

    update goerli deploy script

--- dao/scripts/goerli_deploy.py
+++ dao/scripts/goerli_deploy.py
@@ -1,13 +1,15 @@
 from brownie import *
+from brownie.convert import EthAddress
+from brownie.network import priority_fee
 from pathlib import Path
 
 import time
 import pytest
 
 def main():
+    priority_fee("80 gwei")
     deps = project.load(  Path.home() / ".brownie" / "packages" / config["dependencies"][0])
     TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
-    TimelockController = deps.TimelockController
 
     owner = accounts.load('goerli-owner')
     deployer = accounts.load('goerli-deployer')
@@ -28,11 +30,20 @@ def main():
             govern_contract, deployer, b'',
             {'from': deployer}, publish_source=True)
 
+    timelock = TimeLock.deploy(
+            86400*1,
+            [govern_proxy.address],
+            ["0x0000000000000000000000000000000000000000"],
+            owner,
+            {'from': deployer}, publish_source=True)
+
     transparent_token = Contract.from_abi("BedrockDAO", token_proxy.address, BedrockDAO.abi)
-    transparent_token.initialize( {'from': owner})
-
     transparent_govern = Contract.from_abi("BedrockGovernor", govern_proxy.address, BedrockGovernor.abi)
-    transparent_govern.initialize(transparent_token, {'from': owner})
 
-    print("token address:", transparent_token)
-    print("govern address:", transparent_govern)
+    print("TOKEN ADDRESS:", transparent_token)
+    print("GOVERN ADDRESS:", transparent_govern)
+    print("TIMELOCK ADDRESS:", timelock)
+
+    transparent_token.initialize( {'from': owner})
+    transparent_govern.initialize(transparent_token, timelock, {'from': owner})
+
