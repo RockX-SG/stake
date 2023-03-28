4ce09ce105575aac0cc253c5ca4a4dfcbaf4f5d6
fuli <fuli@rockx.com>
2023-03-28 14:00:10 +0800

    upd bedrock dao token

--- dao/contracts/token.sol
+++ dao/contracts/token.sol
@@ -7,10 +7,9 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20Snapshot
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract BedrockDAOToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable {
+contract BedrockDAO is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -23,9 +22,8 @@ contract BedrockDAOToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgrad
         __Ownable_init();
         __Pausable_init();
         __ERC20Permit_init("Bedrock DAO");
-        __ERC20Votes_init();
 
-        _mint(msg.sender, 10000 * 10 ** decimals());
+        _mint(msg.sender, 100000000 * 10 ** decimals());
     }
 
     function snapshot() public onlyOwner {
@@ -50,28 +48,5 @@ contract BedrockDAOToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgrad
         override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
     {
         super._beforeTokenTransfer(from, to, amount);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._burn(account, amount);
     }
 }
