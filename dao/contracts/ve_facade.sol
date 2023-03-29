f58e849327d2e3a6b2e87300d12d8a01fec7c9a8
fuli <fuli@rockx.com>
2023-03-29 16:12:36 +0800

    add ve_facade

--- dao/contracts/ve_facade.sol
+++ dao/contracts/ve_facade.sol
@@ -15,36 +15,38 @@
 pragma solidity ^0.8.9;
 
 import "interfaces/IVotingEscrow.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
-
+import "@openzeppelin/contracts/utils/Address.sol";
 
 /**
  * @title Rockx Liquid Staking Voting Escrow Facade
- * @author this contract aggregates all balances & totalSupplies of voting escrow
+ * @author RockX Team
+ * @notice this contract aggregates all balances & totalSupplies of voting escrow contracts.
  */
 
-contract VotingEscrowFacade is Initializable, AccessControlUpgradeable, ReentrancyGuardUpgradeable {
+contract VotingEscrowFacade is Initializable, AccessControlUpgradeable {
     bytes32 public constant AUTHORIZED_LOCKER_ROLE = keccak256("AUTHORIZED_LOCKER_ROLE");
+    using Address for address;
     
     // Voting token - Checkpointed view only ERC20
     string public name;
     string public symbol;
     uint256 public decimals;
 
+    address [] public pools;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
- function initialize(
+    function initialize(
         string memory _name,
         string memory _symbol,
         uint256 _decimals) initializer public {
         __AccessControl_init();
-        __ReentrancyGuard_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
@@ -53,4 +55,86 @@ contract VotingEscrowFacade is Initializable, AccessControlUpgradeable, Reentran
         symbol = _symbol;
         decimals = _decimals;
     }
+
+    /**
+     * @dev add a ve pool for aggregation of balance & total supply
+     */
+    function addVePool(address newPool) external
+        onlyRole(DEFAULT_ADMIN_ROLE) {
+
+        require (newPool.isContract(), "MUST_BE_CONTRACT");
+        for (uint i=0;i<pools.length;i++) {
+            if (pools[i] == newPool) {
+                revert("ALREADY_IN");
+            }
+        }
+
+        pools.push(newPool);
+    }
+
+    /**
+     * @notice Sum current user voting power
+     * @param _account User for which to return the voting power
+     * @return power Voting power of user
+     */
+    function balanceOf(address _account) 
+        external 
+        view 
+        returns (uint256 power) {
+            
+        for (uint i=0;i<pools.length;i++) {
+            power += IVotingEscrow(pools[i]).balanceOf(_account);
+        }
+        return power;
+    }
+
+    /**
+     * @notice Sum users voting power at a given blockNumber
+     * @param _account User for which to return the voting power
+     * @param _blockNumber Block at which to calculate voting power
+     * @return power uint256 Voting power of user
+     */
+    function balanceOfAt(address _account, uint256 _blockNumber)
+        external
+        view
+        returns (uint256 power) {
+        
+        for (uint i=0;i<pools.length;i++) {
+            power += IVotingEscrow(pools[i]).balanceOfAt(_account, _blockNumber);
+        }
+        return power;
+    }
+
+    /**
+     * @notice Calculate current total supply of voting power
+     * @return Current totalSupply
+     */
+    function totalSupply() 
+        external 
+        view 
+        returns (uint256) {
+        
+        uint256 ts;
+        for (uint i=0;i<pools.length;i++) {
+            ts += IVotingEscrow(pools[i]).totalSupply();
+        }
+        return ts;
+    }
+
+    /// @notice Calculate total supply of voting power at a given blockNumber
+    /// @param _blockNumber Block number at which to calculate total supply
+    /// @return totalSupply of voting power at the given blockNumber
+    function totalSupplyAt(uint256 _blockNumber)
+        external
+        view
+        returns (uint256) {
+        
+        uint256 ts;
+        for (uint i=0;i<pools.length;i++) {
+            ts += IVotingEscrow(pools[i]).totalSupplyAt(_blockNumber);
+        }
+        return ts;
+    }
+
+    event PoolAdded(address account);
 }
