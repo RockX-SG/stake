15489349ad2f7ff60acb2ab828fb0a07112aa1ba
fuli <fuli@rockx.com>
2023-04-19 20:13:39 +0800

    upd deploy script

--- src/scripts/rate_deploy.py
+++ src/scripts/rate_deploy.py
@@ -2,10 +2,10 @@ from brownie import *
 from pathlib import Path
 
 def main():
-    deployer = accounts[1]
+    deployer = accounts.load('mainnet-deployer')
     rate_proxy = BalancerRateProxy.deploy(
             "0x4beFa2aA9c305238AA3E0b5D17eB20C045269E9d",
-            {'from': deployer}
+            {'from': deployer}, publish_source=True
             )
 
 
