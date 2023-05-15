a3c511d47d77ae84bec857059dd59648ce1254be
fuli <fuli@rockx.com>
2023-05-15 16:43:15 +0800

    added tests for registerValidators

--- src/tests/test_staking.py
+++ src/tests/test_staking.py
@@ -7,16 +7,26 @@ from brownie import convert
 from brownie import *
 
 """ test of registering a validator """
-def test_registerValidator(setup_contracts, owner):
+def test_registerValidator(setup_contracts, owner, pubkeys, sigs):
     transparent_xeth, transparent_staking, transparent_redeem = setup_contracts
     print(transparent_xeth, transparent_staking, transparent_redeem)
 
-    pubkey = 0x97d717d346868b9df4851684d5219f4deb4c7388ee1454c9b46837d29b40150ceeb5825d791f993b03745427b6cbe6db
-    sig = 0xa09b4dc28c10063f6e2a9d2ca94b23db029ef618660138898cb827eae227d99ee1c438988d0222ca4229ba85c40add3b045e823fdb7519a36538ff901ab89f311060bcecc517ba683b84009ee3509afbcd25e991ef34112a5a16be44265441eb
-
-    transparent_staking.registerValidator(pubkey, sig,{'from': owner})
+    transparent_staking.registerValidator(pubkeys[0], sigs[0], {'from': owner})
 
     assert transparent_staking.getRegisteredValidatorsCount() == 1
     results = transparent_staking.getRegisteredValidators(0, 1)
-    assert(results[0][0] == hex(pubkey))
-    assert(results[1][0] == hex(sig))
+    assert(results["pubkeys"][0] == hex(pubkeys[0]))
+    assert(results["signatures"][0] == hex(sigs[0]))
+
+""" test of registering validators """
+def test_registerValidators(setup_contracts, owner, pubkeys, sigs):
+    transparent_xeth, transparent_staking, transparent_redeem = setup_contracts
+    print(transparent_xeth, transparent_staking, transparent_redeem)
+
+    transparent_staking.registerValidators(pubkeys, sigs, {'from': owner})
+
+    assert transparent_staking.getRegisteredValidatorsCount() == len(pubkeys)
+    results = transparent_staking.getRegisteredValidators(0, len(pubkeys))
+    for i in range(len(pubkeys)):
+        assert(results["pubkeys"][i] == hex(pubkeys[i]))
+        assert(results["signatures"][i] == hex(sigs[i]))
