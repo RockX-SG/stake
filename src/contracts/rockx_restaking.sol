061a18599c6f8f40cd0ca4fd0558186dd1ce7c42
fuli <fuli@rockx.com>
2024-01-28 17:03:23 +0800

    make claimDelayedWithdrawals public

--- src/contracts/rockx_restaking.sol
+++ src/contracts/rockx_restaking.sol
@@ -147,7 +147,7 @@ contract RockXRestaking is Initializable, AccessControlUpgradeable, ReentrancyGu
      */
     function claimDelayedWithdrawals(
         uint256 maxNumberOfWithdrawalsToClaim
-    ) external onlyRole(OPERATOR_ROLE) nonReentrant {
+    ) external nonReentrant {
         uint256 balanceBefore = address(this).balance;
         IDelayedWithdrawalRouter(delayedWithdrawalRouter).claimDelayedWithdrawals(maxNumberOfWithdrawalsToClaim);
         uint256 diff = address(this).balance - balanceBefore;
