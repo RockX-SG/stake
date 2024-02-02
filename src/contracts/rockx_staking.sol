5a8304eca404ac3888c2d7057d7ede699cd8b748
fuli <fuli@rockx.com>
2024-02-02 09:40:59 +0800

    fix default limit

--- src/contracts/rockx_staking.sol
+++ src/contracts/rockx_staking.sol
@@ -562,7 +562,8 @@ contract RockXStaking is Initializable, PausableUpgradeable, AccessControlUpgrad
      * with default appreciation limit
      */
     function pushBeacon(uint256 _aliveValidators, uint256, bytes32 clock) external onlyRole(ORACLE_ROLE) {
-        _pushBeacon(_aliveValidators, clock, 5);
+        uint256 limit = 1000 * 32 ether/currentReserve() ;
+        _pushBeacon(_aliveValidators, clock, limit);
     }
 
     /**
