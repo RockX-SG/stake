a4b93df7075724ff9f14e50a3e81e383ffa898ff
Trisome-Bedrock <trisome@bedrock-technology>
2024-06-07 16:20:31 +0800

    restaking: check non-beacon chain pod balance

--- src/contracts/restaking.sol
+++ src/contracts/restaking.sol
@@ -254,6 +254,24 @@ contract Restaking is Initializable, AccessControlUpgradeable, ReentrancyGuardUp
         podOwners.push(podOwner);
     }
 
+    function withdrawNonBeaconChainBalance() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        uint256 totalDiff;
+
+        for (uint256 i=0;i< podOwners.length;i++) {
+            IPodOwner podOwner = podOwners[i];
+            address pod = address(IEigenPodManager(eigenPodManager).getPod(address(podOwner)));
+
+            uint256 diff = IEigenPod(pod).nonBeaconChainETHBalanceWei();
+            if (diff > 0) {
+                podOwner.execute(pod, abi.encodeWithSelector(IEigenPod.withdrawNonBeaconChainETHBalanceWei.selector, podOwner, diff));
+                totalDiff += diff;
+            }
+        }
+
+        pendingWithdrawal += totalDiff;
+        emit Pending(totalDiff);
+    }
+
     /**
      * ======================================================================================
      * 
