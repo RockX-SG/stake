af37a3dc88ae2c44ab288cf5ffcf18bcda63e31d
xynor <ace.xinxuwang@gmail.com>
2025-11-12 14:23:52 +0800

    mocks

---
+++ pectra/contracts/mocks/redeem.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "interfaces/iface.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+contract Redeem is IRedeem, Initializable, PausableUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
+    using Address for address payable;
+
+    mapping(address => uint256) private balances;
+    uint256 private totalBalance;
+
+    constructor() {
+        _disableInitializers();
+    }
+    /**
+     * @dev initialization
+     */
+
+    function initialize() public initializer {
+        __Ownable_init();
+        __Pausable_init();
+        __ReentrancyGuard_init();
+    }
+
+    // some convenient method to help show their claimable in wallet
+    function name() external pure returns (string memory) {
+        return "Bedrock Claimable ETH";
+    }
+
+    function symbol() external pure returns (string memory) {
+        return "redeemETH";
+    }
+
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return totalBalance;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+
+    function claim(uint256 amount) public returns (bool success) {
+        return claim(msg.sender, amount);
+    }
+
+    function claim(address to, uint256 amount) public nonReentrant returns (bool success) {
+        // check
+        require(balances[msg.sender] >= amount, "INSUFFICIENT_BALANCE");
+
+        // modify
+        balances[msg.sender] -= amount;
+        totalBalance -= amount;
+        payable(to).sendValue(amount);
+
+        // log
+        emit Claimed(msg.sender, to, amount);
+        return true;
+    }
+
+    /**
+     * @dev pay debts from rockx staking contract
+     */
+    function pay(address account) external payable override {
+        balances[account] += msg.value;
+        totalBalance += msg.value;
+
+        // log
+        emit Paied(account, msg.value);
+    }
+
+    event Paied(address account, uint256 amount);
+    event Claimed(address indexed _from, address indexed _to, uint256 _value);
+}
